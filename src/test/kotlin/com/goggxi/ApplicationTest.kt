8b87362761ba11ecfce6120726e67c4ab226b26f
goggxi <goggxi@gmail.com>
2022-03-23 15:48:30 +0800

    add unit test all heroes endpoint

--- src/test/kotlin/com/goggxi/ApplicationTest.kt
+++ src/test/kotlin/com/goggxi/ApplicationTest.kt
@@ -1,24 +1,59 @@
 package com.goggxi
 
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.serialization.*
-import io.ktor.features.*
-import org.slf4j.event.*
-import io.ktor.request.*
+import com.goggxi.model.HeroResponse
+import com.goggxi.repository.HeroRepository
+import com.goggxi.repository.HeroRepositoryImpl
 import io.ktor.application.*
-import io.ktor.response.*
-import kotlin.test.*
+import io.ktor.http.*
 import io.ktor.server.testing.*
-import com.goggxi.plugins.*
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.koin.java.KoinJavaComponent.inject
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
+
+    private val repo : HeroRepository by inject(HeroRepositoryImpl::class.java)
+
     @Test
     fun testRoot() {
-        withTestApplication({ configureRouting() }) {
+        withTestApplication(moduleFunction = Application::module) {
             handleRequest(HttpMethod.Get, "/").apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("Hello World!", response.content)
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status()
+                )
+                assertEquals(
+                    expected = "Welcome to Boruto API",
+                    actual = response.content
+                )
+            }
+        }
+    }
+
+    @Test
+    fun `access all heroes endpoint, assert correct information`() {
+        withTestApplication(moduleFunction = Application::module) {
+            handleRequest(HttpMethod.Get, "/boruto/heroes").apply {
+                assertEquals(
+                    expected = HttpStatusCode.OK,
+                    actual = response.status()
+                )
+                val expected = HeroResponse(
+                    success = true,
+                    message = "ok",
+                    prevPage = null,
+                    nextPage = 2,
+                    heroes = repo.page1,
+                )
+
+                val actual = Json.decodeFromString<HeroResponse>(response.content.toString())
+
+                assertEquals(
+                    expected = expected,
+                    actual = actual,
+                )
             }
         }
     }
